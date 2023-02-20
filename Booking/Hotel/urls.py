a3fceb28bf7e0e3453057f0a9c190077745e2200
Diksha Magotra <dikshamagotra2021@gmail.com>
2023-02-20 13:02:26 -0500

    Removed login/reg views

--- Booking/Hotel/urls.py
+++ Booking/Hotel/urls.py
@@ -7,10 +7,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('check_booking/' , check_booking),
     path('', home, name='home'),
-    path('hotel-detail/<uid>/' , hotel_detail , name="hotel_detail"),
-    path('login/', login_page , name='login_page'),
-    path('register/', register_page , name='register_page'),
-         
+    path('hotel-detail/<uid>/' , hotel_detail , name="hotel_detail"),         
 ]
 
 
