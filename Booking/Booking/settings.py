a213f1ba511a5d70b1180e8f69a773884c47f43e
Diksha Magotra <dikshamagotra2021@gmail.com>
2023-03-10 17:07:18 -0500

    Authentication Works

--- Booking/Booking/settings.py
+++ Booking/Booking/settings.py
@@ -1,5 +1,6 @@
 try:
     from Booking.env_settings import *
+    from Booking.email_settings import *
     from Booking.static_settings import *
     from Booking.templates_settings import *
     from Booking.database_settings import *
@@ -15,5 +16,4 @@ except ImportError:
     pass
 
 SECRET_KEY = 'django-insecure-n15)bjerq(w7rl+v-=ivq2o%l4kl+em9z-69-hejs64a0tf8&u'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-ACCOUNT_EMAIL_VERIFICATION = "none"
+
