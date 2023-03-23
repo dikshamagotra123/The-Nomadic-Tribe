036bd4210a16aa8438662d29efeb9cb8741d1de5
“sakshamtandon” <saksham12356@gmail.com>
2023-03-23 15:11:11 -0400

    Updated email and templates

--- Booking/load_data.sh
+++ Booking/load_data.sh
@@ -2,3 +2,4 @@
 python manage.py loaddata fixtures/amentities.json 
 python manage.py loaddata fixtures/adventures.json
 python manage.py loaddata fixtures/hotels.json
+python manage.py loaddata fixtures/hotel_images.json
